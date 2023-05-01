1af5ee6029bee13322f03c7704c2c86466e9a2dd
bjlli <brendajacomelli@gmail.com>
2023-05-01 15:55:43 -0300

    hw bare metal .hex implementation

---
+++ hw/hex/hex2bin.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *hex_file, *bin_file;
+    int val;
+
+    hex_file = fopen("hw.hex", "r");
+
+    if(hex_file == NULL){
+        printf("Error opening .hex file");
+        return 0;
+    }
+
+    bin_file = fopen("hw.bin", "wb");
+
+    if(hex_file == NULL){
+        printf("Error opening .bin file");
+        return 0;
+    }
+
+    while ( fscanf (hex_file, "%2x", &val) >= 0){
+        fputc (val, bin_file);
+    }
+      
+
+    fclose(hex_file);
+    fclose(bin_file);
+
+    return 0;
+}
