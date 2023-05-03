399a2857951cd4478f541a0de2df30e7235ee159
bjlli <brendajacomelli@gmail.com>
2023-05-03 20:29:40 -0300

    plain c printf tests

--- hw/plain-c/hw_utils.c
+++ hw/plain-c/hw_utils.c
@@ -1,18 +1,18 @@
 
 
-int __attribute__((fastcall, naked))  printf(const char* s)
+int __attribute__((thiscall, naked))  printf(const char* s)
 {
 __asm__
 (
-"   mov   %cx, %bx             \n"
+"   mov   %cx, %bx            \n"
 "	mov   $0x0e, %ah           \n"
 "	mov   $0x0, %si            \n"
 "loop:	           	           \n"
 "	mov   (%bx, %si), %al      \n"
-"	cmp   $0x0, %al	           \n"
+"	cmp   $0x0, %al            \n"
 "	je    end                  \n"
 "	int   $0x10	           \n"
-"	add   $0x1, %si 	   \n"
+"	inc   %bx	           \n"
 "	jmp   loop	           \n"
 "end:                              \n"
 "        mov   %si, %ax            \n"
